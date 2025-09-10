292caa2fd6e9e784605069f4fcc832b2b6e527e0
Krishna Agarwal <70256295+anhsirkkrishna@users.noreply.github.com>
2025-09-10 11:56:24 +0530

    Create -- make_it_equal.py

---
+++ -- make_it_equal.py
@@ -0,0 +1,6 @@
+I wrote the code but it was very time consuming solution and I was getting TLE again and again.
+But I used map, unodered map, and many things to make the program more efficient.
+Something I could have done is to use ai less and think.
+But the problem was so hard that I couldn't.
+Still, a big part of creating the code efficient was thought by me not ai which is very good.
+Link: https://codeforces.com/problemset/problem/2131/C
